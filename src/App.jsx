6a1c483b86cdab162369b6241dbdf678c83c38e9
tecnicorikardo <tecnicorikardo@gmail.com>
2024-10-23 18:53:30 -0300

    update index

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,9 @@ export const App = () => (
     <p>Sum of 0 and 5 is 5</p>
     <p>Sum of 0 and 0 is 0</p>
     {/* Replace paragraphs with Sum componets */}
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium eaque in modi porro non, totam vitae tenetur repudiandae et esse nesciunt voluptas praesentium autem ad incidunt quaerat sint dicta ducimus.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem labore, numquam iste delectus repudiandae dolore amet qui, saepe architecto minus deserunt ratione dignissimos distinctio, quisquam sapiente voluptatem blanditiis natus. Fuga?
+    </p>
     {/* And remove commented lines :) */}
   </>
 );
