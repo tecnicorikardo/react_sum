d957432ed6d539eb24f99e4eef3ee541631a4219
tecnicorikardo <tecnicorikardo@gmail.com>
2024-10-24 07:26:15 -0300

    Feita as alterações dos arquivos JSX

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,1 +1,15 @@
 // export const Sum = () => ();
+import React from 'react';
+
+function Sum({ a = 0, b = 0 }) {
+  const sum = a + b;
+
+  return (
+    <div>
+      <h1>Soma</h1>
+      <p>A soma de {a} e {b} é {sum}</p>
+    </div>
+  );
+}
+
+export default Sum; // Exportação padrão
