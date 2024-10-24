8764171f4a44cd0d77fb9aad18a256fa7a0c1c2b
tecnicorikardo <tecnicorikardo@gmail.com>
2024-10-24 07:54:33 -0300

    Feita as alterações JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
 import React from 'react';
 import './App.scss';
+import { Sum } from './components/Sum/Sum';
 
 export const App = () => (
   <>
-    <p>Sum of 2 and 3 is 5</p>
-    <p>Sum of -5 and 5 is 0</p>
-    <p>Sum of 10 and 0 is 10</p>
-    <p>Sum of 0 and 5 is 5</p>
-    <p>Sum of 0 and 0 is 0</p>
-    {/* Replace paragraphs with Sum componets */}
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium eaque in modi porro non, totam vitae tenetur repudiandae et esse nesciunt voluptas praesentium autem ad incidunt quaerat sint dicta ducimus.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem labore, numquam iste delectus repudiandae dolore amet qui, saepe architecto minus deserunt ratione dignissimos distinctio, quisquam sapiente voluptatem blanditiis natus. Fuga?
-    </p>
-    {/* And remove commented lines :) */}
+    <Sum a={2} b={3} />
+    <Sum a={-5} b={5} />
+    <Sum a={10} />
+    <Sum b={5} />
+    <Sum />
   </>
 );
